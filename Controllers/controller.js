31c51a090c1c15072665b82d7e34f31148fbe17f
Imam bahri alwi <imambahrialwi21@gmail.com>
2024-12-07 14:40:59 +0700

    Validate register dan login

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -9,10 +9,9 @@ class Controller {
         try {
             const { error } = req.query
             let data = await Profile.findAll()
-            res.render('login', { data ,error})
+            res.render('login', { data, error })
         } catch (error) {
-
-            console.log(error);
+            // console.log(error);
             res.send(error)
         }
     }
@@ -20,7 +19,7 @@ class Controller {
         try {
             const { username, password } = req.body
             const user = await User.findOne({ where: { username } });
-            
+
             // console.log('Username:', username);
             // console.log('Password:', password);
 
@@ -29,24 +28,19 @@ class Controller {
                 if (isValidPassword) {
                     req.session.UserId = user.id
                     res.redirect('/mainPage');
-
-
-
                     // console.log('Session User ID:', req.session.UserId)
                 } else {
                     const error = 'Invalid username or password';
                     return res.redirect(`/login?error=${error}`);
                 }
             } else {
-                const error = 'Invalid username or password';
+                const error = 'Username is not found, please create account';
                 return res.redirect(`/login?error=${error}`);
             }
         } catch (error) {
             res.send(error)
         }
     }
-
-
 
 
     // Register
@@ -56,32 +50,38 @@ class Controller {
             if (errors) {
                 errors = errors.split(',');
             }
-
-            // res.send(errors)
             res.render('register', { errors })
 
         } catch (error) {
-            console.log(error);
-
             res.send(error)
         }
     }
 
     static async registerPagePost(req, res) {
         try {
-           let role = 'user'
-            const { username, email, password} = req.body
+            let role = 'user'
+
+            const { username, email, password } = req.body
+            const user = await User.findOne({ where: { username } });
+
+            if (user) {
+                const errors = 'Username already exists'
+                return res.redirect(`/register?errors=${errors}`);
+            }
+
+            // if (password.length < 8) {
+            //    const errors = `Password must be more than 8`
+            //    return res.redirect(`/register?errors=${errors}`);
+            // }
+
             await User.create({ username, email, password, role })
 
             res.redirect('/login')
         } catch (error) {
             console.log(error);
-            
             if (error.name === "SequelizeValidationError") {
-                // res.send(error) 
 
                 const erorrs = error.errors.map((el) => el.message)
-
                 res.redirect(`/register?errors=${erorrs}`)
             } else {
                 res.send(error)
@@ -93,7 +93,8 @@ class Controller {
     }
 
 
-    static async mainPage(req,res){
+    //Main Page
+    static async mainPage(req, res) {
         try {
             res.render('mainPage')
         } catch (error) {
